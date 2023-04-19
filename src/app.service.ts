8bcd435def0fe9b3522316454c81f46d6d0cfbed
Islam Music <islam@Islams-MacBook-Pro.local>
2023-04-19 08:41:35 +0200

    prvi commit

---
+++ src/app.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'ERŠ je najjača šola!!!!!';
+  }
+}
---
+++ src/user/dto/update-user.dto.ts
@@ -0,0 +1,7 @@
+import { IsOptional } from 'class-validator';
+export class UpdateUserDto {
+  @IsOptional()
+  first_name?: string;
+  @IsOptional()
+  last_name?: string;
+}
